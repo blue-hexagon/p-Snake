2cc82adb2580f2e4fbc27566a59ec8aedda4c686
Allan <w0j8uhv5csio@opayq.net>
2021-04-18 00:03:58 +0200

    Make the scoreboard save and retrieve data from a textfile with the scores, in between application sessions.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import curses
 
 import menu
+import scores
 import snake
 
 if __name__ == '__main__':
     while True:
+        scores.load_scores()
         curses.wrapper(menu.main)
         curses.wrapper(snake.main)
--- scores.py
+++ scores.py
@@ -1,15 +1,44 @@
-scores = [['Blaze', 1000], ['Fast Eddy', 900], ['Starcat', 800], ['Hammerhead', 700], ['Mad Hatter', 600],
-          ['Primus', 500], ['Tempest', 400], ['Blackbird', 300], ['DragonHawk', 200], ['Wonderboy', 100]]
+import shelve
+
+scores = []
+
+""" Used to reinitialize the storage """
+def init_shelve():
+    scores = [['Blaze', 1000], ['Fast Eddy', 900], ['Starcat', 800], ['Hammerhead', 700], ['Mad Hatter', 600],
+              ['Primus', 500], ['Tempest', 400], ['Blackbird', 300], ['DragonHawk', 15], ['Wonderboy', 5]]
+    d = shelve.open('score.txt')
+    for i in range(0,10):
+        d['scores'+str(i)] = scores[i]
+    for i in range(0,10):
+        print("----------------------")
+        print(d['scores'+str(i)])
+        print("----------------------")
+    d.close()
+
+
+def load_scores():
+    d = shelve.open('score.txt')
+    scores.clear()
+    for i in range(0,10):
+        scores.append(d['scores'+str(i)])
+    d.close()
 
 
 def update_score(gamescore, player_name="Player"):
     for idx in enumerate(scores):
         if gamescore >= scores[idx[0]][1]:
+            with shelve.open('score.txt',writeback=True) as d:
+                d['scores'+str(idx[0])][0] = player_name
+                d['scores'+str(idx[0])][1] = gamescore
             scores[idx[0]][1] = gamescore
             scores[idx[0]][0] = player_name
+            load_scores()
             break
 
 
 if __name__ == '__main__':
-    update_score(225)
-    print(scores)
+    pass
+    # init_shelve()
+    # load_scores()
+    # update_score(225)
+    # print(scores)
